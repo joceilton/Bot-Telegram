09527814afdf365af1d038e495fc1ca4109ed5ce
joceilton <joceilton.g@gmail.com>
2021-06-01 08:44:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '1855869727:AAHico6A-Brln3iTZL-owjCr7h0eCLigyqA';
+const token = 'TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
